d08c7fc1f35a753fc807196a0ca860f581a1e134
gfaruk02 <gfaruk02@gmail.com>
2024-01-25 23:56:51 +0600

    create todo list post method and use toast

--- src/Components/Home/CreateTask.jsx
+++ src/Components/Home/CreateTask.jsx
@@ -1,19 +1,34 @@
+import toast, { Toaster } from 'react-hot-toast';
+import useAxios from "../../Hooks/useAxios";
 
 
 const CreateTask = () => {
+    const axiosPublic = useAxios()
     const handleTaskCreate=e=>{
         e.preventDefault();
         const form = e.target
         const title = form.title.value;
         const description = form.description.value;
         const role = "active"
-        const info = {title, description, role}
-        console.log(info);
+        const task = {title, description, role}
+        console.log(task);
+
+        axiosPublic.post('/taskList', task)
+            .then(res=>{ 
+                console.log(res.data);
+                if(res.data.insertedId){
+                    toast.success('Successfully toasted!')
+                }
+                form.reset();
+            })
+            .catch(err=>{
+                toast.error(err)
+            });
     }
     return (
-        <div className="mt-5 md:px-20">
+        <div className="px-2 py-5 border-b-2 border-gray-300 w-full lg:w-10/12 mx-auto">
             <h1 className="text-3xl font-semibold"> Create Task List</h1>
-            <form onSubmit={handleTaskCreate} className="py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 items-center mx-auto">
+            <form onSubmit={handleTaskCreate} className="py-5 grid grid-cols-1 md:grid-cols-3 gap-5 lg:gap-10 items-center mx-auto">
                 <div className="form-control">
                     <label className=" label">
                         <span className="label-text text-xl font-semibold text-gray-950 ">Task Title</span>
@@ -30,6 +45,7 @@ const CreateTask = () => {
                     <button className="btn btn-primary bg-gray-400 border-none text-gray-950 font-bold hover:bg-orange-400"> Add Task</button>
                 </div>
             </form>
+            <Toaster />
         </div>
     );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,20 +3,20 @@ import CreateTask from "../Components/Home/CreateTask";
 
 const Home = () => {
     return (
-        <div className=" w-11/12 mx-auto">
-            <div className="bg-gray-300 text-center">
+        <div className=" lg:w-11/12 mx-auto bg-gray-100">
+            <div className=" text-center bg-gray-200">
                 <h1 className="text-5xl font-bold py-5"> TasK Todo List</h1>
             </div>
             <CreateTask></CreateTask>
 
-            <div role="tablist" className="tabs tabs-lifted w-10/12 mx-auto mt-5">
-                <input type="radio" name="my_tabs_2" role="tab" className="tab ml-20 md:ml-36 text-xl font-semibold text-gray-900" aria-label="View All Tesks" checked/>
-                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+            <div role="tablist" className="px-2 tabs tabs-lifted w-full lg:w-10/12 mx-auto mt-5">
+                <input type="radio" name="my_tabs_2" role="tab" className="tab ml-20 lg:ml-36 text-sm lg:text-xl font-semibold text-gray-900" aria-label="View All Tesks" checked/>
+                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box lg:p-6">
 
                     <div className="overflow-x-auto">
                         <table className="table">
                             <thead>
-                                <tr className="text-lg text-gray-700">
+                                <tr className="text-sm lg:text-lg text-gray-700">
                                     <th>Sl</th>
                                     <th>Title</th>
                                     <th>Description</th>
@@ -31,7 +31,7 @@ const Home = () => {
                                     <td className="flex flex-row gap-2">
                                         <button className=" px-3 py-2 font-semibold bg-green-400 rounded-lg hover:bg-green-800 hover:text-white"> Edit </button>
                                         <button className=" px-3 py-2 font-semibold bg-red-400 rounded-lg hover:bg-red-700 hover:text-white"> Delete</button>
-                                        <button className=" px-3 py-2 font-semibold bg-slate-400 rounded-lg hover:bg-slate-800 hover:text-white"> Complete </button>
+                                        <button className=" px-3 py-2 font-semibold bg-slate-400 rounded-lg hover:bg-slate-800 hover:text-white"> Completed </button>
                                     </td>
                                 </tr>
                                 
@@ -41,8 +41,8 @@ const Home = () => {
 
                 </div>
 
-                <input type="radio" name="my_tabs_2" role="tab" className="tab text-xl font-semibold text-gray-900 px-5" aria-label="Complete Task" />
-                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+                <input type="radio" name="my_tabs_2" role="tab" className="tab text-xl font-semibold text-gray-900 px-2" aria-label="Complete Task" />
+                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box lg:p-6">
 
                 <div className="overflow-x-auto">
                         <table className="table">
