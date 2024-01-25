74d62e6122f0fc7d79cd924ea5a6c42c8f099dee
gfaruk02 <gfaruk02@gmail.com>
2024-01-25 17:02:16 +0600

    design todo list UI view

--- src/Components/Home/CreateTask.jsx
+++ src/Components/Home/CreateTask.jsx
@@ -1,10 +1,19 @@
 
 
 const CreateTask = () => {
+    const handleTaskCreate=e=>{
+        e.preventDefault();
+        const form = e.target
+        const title = form.title.value;
+        const description = form.description.value;
+        const role = "active"
+        const info = {title, description, role}
+        console.log(info);
+    }
     return (
         <div className="mt-5 md:px-20">
             <h1 className="text-3xl font-semibold"> Create Task List</h1>
-            <form className="py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 items-center mx-auto">
+            <form onSubmit={handleTaskCreate} className="py-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10 items-center mx-auto">
                 <div className="form-control">
                     <label className=" label">
                         <span className="label-text text-xl font-semibold text-gray-950 ">Task Title</span>
@@ -15,10 +24,10 @@ const CreateTask = () => {
                     <label className=" label">
                         <span className="label-text text-xl font-semibold text-gray-950">Task Description</span>
                     </label>
-                    <textarea name="description" className="textarea textarea-bordered h-10" placeholder=" Write Tesk Description"></textarea>
+                    <textarea name="description" className="textarea textarea-bordered h-10" placeholder=" Write Tesk Description" required></textarea>
                 </div>
-                <div className="mt-8">
-                    <button className="btn btn-primary"> Add Task</button>
+                <div className="md:mt-9">
+                    <button className="btn btn-primary bg-gray-400 border-none text-gray-950 font-bold hover:bg-orange-400"> Add Task</button>
                 </div>
             </form>
         </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,6 +8,68 @@ const Home = () => {
                 <h1 className="text-5xl font-bold py-5"> TasK Todo List</h1>
             </div>
             <CreateTask></CreateTask>
+
+            <div role="tablist" className="tabs tabs-lifted w-10/12 mx-auto mt-5">
+                <input type="radio" name="my_tabs_2" role="tab" className="tab ml-20 md:ml-36 text-xl font-semibold text-gray-900" aria-label="View All Tesks" checked/>
+                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+
+                    <div className="overflow-x-auto">
+                        <table className="table">
+                            <thead>
+                                <tr className="text-lg text-gray-700">
+                                    <th>Sl</th>
+                                    <th>Title</th>
+                                    <th>Description</th>
+                                    <th>Action Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="hover">
+                                    <th>1</th>
+                                    <td>Cy Ganderton</td>
+                                    <td>Quality Control Specialist</td>
+                                    <td className="flex flex-row gap-2">
+                                        <button className=" px-3 py-2 font-semibold bg-green-400 rounded-lg hover:bg-green-800 hover:text-white"> Edit </button>
+                                        <button className=" px-3 py-2 font-semibold bg-red-400 rounded-lg hover:bg-red-700 hover:text-white"> Delete</button>
+                                        <button className=" px-3 py-2 font-semibold bg-slate-400 rounded-lg hover:bg-slate-800 hover:text-white"> Complete </button>
+                                    </td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+
+                <input type="radio" name="my_tabs_2" role="tab" className="tab text-xl font-semibold text-gray-900 px-5" aria-label="Complete Task" />
+                <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+
+                <div className="overflow-x-auto">
+                        <table className="table">
+                            <thead>
+                                <tr className="text-lg text-gray-700">
+                                    <th>Sl</th>
+                                    <th>Title</th>
+                                    <th>Description</th>
+                                    <th>Action Status</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="hover">
+                                    <th>1</th>
+                                    <td>Cy Ganderton</td>
+                                    <td>Quality Control Specialist</td>
+                                    <td className="flex flex-row gap-2">
+                                        <button className=" px-3 py-2 font-semibold bg-green-400 rounded-lg hover:bg-green-800 hover:text-white"> Edit </button>
+                                        <button className=" px-3 py-2 font-semibold bg-red-400 rounded-lg hover:bg-red-700 hover:text-white"> Delete</button>
+                                    </td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
